90f288fc02dca8065a5cbb4e23421e186aacc24e
n1ret <n1ret.lcode@gmail.com>
2026-09-13 21:47:02 +0500

    Gaming keymap change

--- keymap.c
+++ keymap.c
@@ -46,18 +46,18 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 //    ├──────┼───┼──────┼──────┼─────┼──────┤                 ├───┼───┼─────┼────────────────┼───┼──────┤
 //    │ esc  │ q │  w   │  e   │  r  │  t   │                 │ y │ u │  i  │       o        │ p │ bspc │
 //    ├──────┼───┼──────┼──────┼─────┼──────┤                 ├───┼───┼─────┼────────────────┼───┼──────┤
-//    │ spc  │ a │  s   │  d   │  f  │  g   │                 │ h │ j │  k  │       l        │ ; │  '   │
+//    │ tab  │ a │  s   │  d   │  f  │  g   │                 │ h │ j │  k  │       l        │ ; │  '   │
 //    ├──────┼───┼──────┼──────┼─────┼──────┼──────┐   ┌──────┼───┼───┼─────┼────────────────┼───┼──────┤
-//    │ lsft │ z │  x   │  c   │  v  │  b   │ mute │   │ mply │ n │ m │  ,  │       .        │ / │  =   │
+//    │ lctl │ z │  x   │  c   │  v  │  b   │ mute │   │ mply │ n │ m │  ,  │       .        │ / │  =   │
 //    └──────┴───┼──────┼──────┼─────┼──────┼──────┤   ├──────┼───┼───┼─────┼────────────────┼───┴──────┘
-//               │ lctl │ home │ tab │ lalt │ ent  │   │ lgui │ [ │ ] │ end │ OSL(_ONESHOTS) │
+//               │ lalt │ lsft │ spc │ home │ ent  │   │ lgui │ [ │ ] │ end │ OSL(_ONESHOTS) │
 //               └──────┴──────┴─────┴──────┴──────┘   └──────┴───┴───┴─────┴────────────────┘
 [_GAMING] = LAYOUT(
   KC_GRV  , KC_1 , KC_2    , KC_3    , KC_4   , KC_5    ,                         KC_6    , KC_7    , KC_8    , KC_9           , KC_0    , KC_MINS,
   KC_ESC  , KC_Q , KC_W    , KC_E    , KC_R   , KC_T    ,                         KC_Y    , KC_U    , KC_I    , KC_O           , KC_P    , KC_BSPC,
-  KC_SPC  , KC_A , KC_S    , KC_D    , KC_F   , KC_G    ,                         KC_H    , KC_J    , KC_K    , KC_L           , KC_SCLN , KC_QUOT,
-  KC_LSFT , KC_Z , KC_X    , KC_C    , KC_V   , KC_B    , KC_MUTE ,     KC_MPLY , KC_N    , KC_M    , KC_COMM , KC_DOT         , KC_SLSH , KC_EQL ,
-                   KC_LCTL , KC_HOME , KC_TAB , KC_LALT , KC_ENT  ,     KC_LCMD , KC_LBRC , KC_RBRC , KC_END  , OSL(_ONESHOTS)
+  KC_TAB  , KC_A , KC_S    , KC_D    , KC_F   , KC_G    ,                         KC_H    , KC_J    , KC_K    , KC_L           , KC_SCLN , KC_QUOT,
+  KC_LCTL , KC_Z , KC_X    , KC_C    , KC_V   , KC_B    , KC_MUTE ,     KC_MPLY , KC_N    , KC_M    , KC_COMM , KC_DOT         , KC_SLSH , KC_EQL ,
+                   KC_LALT , KC_LSFT , KC_SPC , KC_HOME , KC_ENT  ,     KC_LCMD , KC_LBRC , KC_RBRC , KC_END  , OSL(_ONESHOTS)
 ),
 
 //    ┌─────┬─────┬─────┬─────┬─────┬─────┐               ┌──────┬─────────────┬─────────────┬──────┬──────┬─────┐
